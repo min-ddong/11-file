a4043a1cefc6f6988b39202852ad2ffa2bb02d21
min-ddong <jellykim907@gmail.com>
2020-11-16 23:05:16 +0900

    fourth

--- first.c
+++ first.c
@@ -2,15 +2,18 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+char *proverb = " All that gliters is not gold."; 
 
-int main(int argc, char *argv[]) {
-int i = 100;
-int*p = &i;
-int**q = &p;
+void setPointer(char **q)
+{
+	*q = proverb;
+}
 
-*p = 200;
-printf("i = %d, *p = %d, **q = %d\n",i,*p, **q);
-**q = 300;
-printf("i = %d, *p = %d, **q = %d\n",i,*p, **q);
+int main(int argc, char *argv[]){
+	
+	char*p = "zzz";
+	setPointer(&p);
+	printf("%s\n", p);
+	
 	return 0;
 }
