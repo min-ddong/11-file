5fec6f891267167cfece152efccab93735bdf146
min-ddong <jellykim907@gmail.com>
2020-11-16 23:33:56 +0900

    fifth

--- first.c
+++ first.c
@@ -2,18 +2,52 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-char *proverb = " All that gliters is not gold."; 
 
-void setPointer(char **q)
+
+void print_image(int a[5][5])
 {
-	*q = proverb;
+	int i, j;
+	int *ptr;
+	
+	ptr = &a[0][0];
+	for (i=0; i<5;i++) { 
+		for(j=0;j<5;j++){
+			printf("%d" ,*ptr);
+			ptr++;
+		}
+		printf("\n");  
+	}
 }
 
-int main(int argc, char *argv[]){
+void brighten_image(int a[5][5])
+{
+	int i, j;
+	int *ptr;
 	
-	char*p = "zzz";
-	setPointer(&p);
-	printf("%s\n", p);
+	ptr = (int*)a[0];
+	for(i=0;i<5;i++){
+		for(j=0;j<5;j++){
+			*ptr += 10;
+			ptr++;
+		}
+		printf("\n");
+	}
+}
+
+
+int main(int argc, char *argv[]) {
+	
+	int image[5][5] = {
+	{10, 20, 30, 40, 50},
+	{10, 20, 30, 40, 50},
+	{10, 20, 30, 40, 50},
+	{10, 20, 30, 40, 50},
+	{10, 20, 30, 40, 50},
+	};
+	
+	print_image(image);
+	brighten_image(image);
+	print_image(image);
 	
 	return 0;
 }
